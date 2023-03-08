c67aa8558cd749d7629b78545ab5147a3cf209a5
jiasir <jiasir@icloud.com>
2023-03-08 15:13:08 +0800

    Google OAuth2

--- grace_manager/app.py
+++ grace_manager/app.py
@@ -1,9 +1,34 @@
+import json, os, sqlite3
 from flask import Flask, request, redirect, url_for
 from flask_oauthlib.client import OAuth
 from flask_socketio import SocketIO, emit
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# configuration
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 # Create the flask app
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24)
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 # Secret key for session management
 app.config['SECRET_KEY'] = 'secret!'
@@ -34,10 +59,12 @@ google = oauth.remote_app(
     authorize_url='https://accounts.google.com/o/oauth2/auth'
 )
 
+
 @app.route('/oauth/login')
 def login():
     callback_url = url_for('oauth_authorized', provider='google', _external=True)
     return oauth.authorize_redirect(callback=callback_url, provider='google')
+
 
 @app.route('/oauth/authorized')
 def oauth_authorized():
@@ -50,21 +77,24 @@ def oauth_authorized():
     access_token = response['access_token']
     return 'Access granted!'
 
+
 @app.route('/v1/chat', methods=['POST'])
 @oauth.require_oauth('google')
 def send_message():
     # Get the message form the request
-    #message = request.json['message']
+    # message = request.json['message']
     message = request.json
 
     # Stream the message to all connected clients
     socketio.emit('message', {'message': message})
-    
+
     # Return a success response
     return {'status': 'success'}
 
+
 # stream all previous messages to the client by emitting a message event for each message in the message_history list
 message_history = []
+
 
 @app.route('/v1/chat', methods=['GET'])
 @oauth.require_oauth('google')
@@ -77,6 +107,7 @@ def receive_message():
 
     # Return a success response
     return {'status': 'success'}
+
 
 # run the app
 if __name__ == '__main__':
