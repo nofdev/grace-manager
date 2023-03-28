a113491420e259bf8b8c7a17bb012a242b232ada
jiasir <jiasir@icloud.com>
2023-03-28 09:20:24 +0800

    Format

--- frontend/chat.js
+++ frontend/chat.js
@@ -1,13 +1,21 @@
 const input = document.getElementById('input');
 const sendBtn = document.getElementById('send');
+const resultDiv = document.getElementById('result');
 
 function sendMessage() {
     const message = input.value;
 
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message;
+
     // Send HTTP GET request to API endpoint, and print result in response
     fetch(`/api/sendMessage?message=${message}`)
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => {
+            const resultElement = document.createElement('p');
+            resultElement.textContent = result.message;
+            resultDiv.appendChild(resultElement);
+        })
         .catch(error => console.error(error));
 
     input.value = '';
