5c632bbbed909fcb283e2f718a4912698c58e587
jiasir <jiasir@icloud.com>
2023-03-28 14:32:30 +0800

    Fix message format

--- frontend/chat.js
+++ frontend/chat.js
@@ -5,13 +5,18 @@ const resultDiv = document.getElementById('result');
 function sendMessage() {
     const message = input.value;
 
-    const messageElement = document.createElement('p');
-    messageElement.textContent = message;
+    // const messageElement = document.createElement('p');
+    // messageElement.textContent = message;
 
     const requestBody = {
         model: 'gpt-3.5-turbo',
         messages: [{role: 'user', content: message}],
     }
+
+    // Control input message to display
+    const resultElement = document.createElement('p');
+    resultElement.textContent = message
+    resultDiv.appendChild(resultElement);
 
     // Send HTTP GET request to API endpoint, and print result in response
     // Send to Gateway API DO NOT Sent to OpenAI API, Only Dev environment
@@ -34,6 +39,7 @@ function sendMessage() {
             resultElement.textContent = result.choices[0].message.content;
 
             resultDiv.appendChild(resultElement);
+            resultDiv.appendChild(document.createElement('br'));
         })
         .catch(error => console.error(error));
 
