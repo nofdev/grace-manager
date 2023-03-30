ad40766a9ded19b6e3a119a0461a19a0ce870cfd
jiasir <jiasir@icloud.com>
2023-03-30 19:18:16 +0800

    Add gateway

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -1,116 +1,58 @@
-// This snippet from gateway/gateway.js is the gateway between the frontend and the backend
-// Running on Node.js
-
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
-const library = require('./library');
+const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Handle GET requests under the /endpoint path
-app.get('/endpoint', (req, res) => {
-    try {
-        // Call the library function to process the request
-        const result = library.processRequest(req.query);
+app.use(cors());
+app.use(express.json());
 
-        // Send the result back to the client
-        res.json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+const privateKey = fs.readFileSync('/root/cert/api.jiasir.io.key', 'utf8');
+const certificate = fs.readFileSync('/root/cert/api.jiasir.io.cert', 'utf8');
+const ca = fs.readFileSync('/root/cert/api.jiasir.io.fullchain.pem', 'utf8');
 
-app.post('/endpoint', (req, res) => {
-    try {
-        // Call the library function to process the request
-        const result = library.processRequest(req.body);
-
-        // Send the result back to the client
-        res.json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-})
-
-// OAuth 2.0
-const axios = require('axios');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
-
-// Middleware for parsing JSON request body
-app.use(bodyParser.json());
-
-// Secret key for JWT token
-const secretKey = 'my-secret-key';
-
-// Route to get access token
-app.post('/auth/token', async (req, res) => {
-    try {
-        // Check client credentials (in this example, hardcoded)
-        const { clientId, clientSecret } = req.body;
-        if (clientId !== 'my-client-id' || clientSecret !== 'my-client-secret') {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
-        // Generate JWT access token with expiration time of 1 hour
-        const accessToken = jwt.sign({ sub: 'my-user-id' }, secretKey, { expiresIn: '1h' });
-
-        // Return access token
-        res.json({ access_token: accessToken });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-// Middleware to authenticate user with access token
-const authenticateUser = async (req, res, next) => {
-    try {
-        // Get access token from Authorization header
-        const authorizationHeader = req.headers.authorization;
-        if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-        const accessToken = authorizationHeader.substring(7);
-
-        // Verify JWT access token and get user ID
-        const decodedAccessToken = jwt.verify(accessToken, secretKey);
-        req.userId = decodedAccessToken.sub;
-
-        next();
-    } catch (error) {
-        console.error(error);
-        res.status(401).json({ error: 'Unauthorized' });
-    }
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
 };
 
-// Route to forward API requests to backend
-app.all('/api/*', authenticateUser, async (req, res) => {
-    try {
-        // Get backend API URL
-        const backendUrl = 'http://localhost:8000' + req.url.substring(4);
+const httpsServer = https.createServer(credentials, app);
 
-        // Forward request to backend API with user ID in headers
-        const response = await axios({
-            method: req.method,
-            url: backendUrl,
-            data: req.body,
+app.post('/chat', async (req, res) => {
+    try {
+        const { model, messages, temperature, top_p, n, stream, stop, max_tokens, presence_penalty, frequency_penalty, logit_bias, user } = req.body;
+
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+            model,
+            messages,
+            temperature,
+            top_p,
+            n,
+            stream,
+            stop,
+            max_tokens,
+            presence_penalty,
+            frequency_penalty,
+            logit_bias,
+            user
+        }, {
             headers: {
-                'X-User-Id': req.userId,
-            },
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer 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` // replace with your API key
+            }
         });
 
-        // Forward response from backend API to client
-        res.status(response.status).json(response.data);
+        res.send(response.data);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send('Something went wrong');
     }
 });
 
-// Start API gateway
-app.listen(port, () => {
-    console.log(`API Gateway listening at http://localhost:${port}`);
+httpsServer.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
