a18508396a9cc76a6f3f35383e40ada73e2444eb
jiasir <jiasir@icloud.com>
2023-04-05 01:17:26 +0800

    Refactor gateway.go

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,30 +1,125 @@
 package main
 
 import (
-	"net/http/httputil"
-	"net/url"
-
-	"github.com/gin-gonic/gin"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+type RequestBody struct {
+	Model            string    `json:"model"`
+	Messages         []string  `json:"messages"`
+	Temperature      float64   `json:"temperature"`
+	TopP             float64   `json:"top_p"`
+	N                int       `json:"n"`
+	Stream           bool      `json:"stream"`
+	Stop             []string  `json:"stop"`
+	MaxTokens        int       `json:"max_tokens"`
+	PresencePenalty  float64   `json:"presence_penalty"`
+	FrequencyPenalty float64   `json:"frequency_penalty"`
+	LogitBias        []float64 `json:"logit_bias"`
+	User             string    `json:"user"`
+}
+
+type ResponseBody struct {
+	Choices []struct {
+		Text string `json:"text"`
+	} `json:"choices"`
+}
+
 func main() {
-	// Create proxy instance and setting target address
-	targetURL, _ := url.Parse("http://localhost:8080")
-	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+	http.HandleFunc("/chat", handleChat)
 
-	// Create Gin instance
-	router := gin.Default()
-
-	// Define reverse proxy middleware
-	reverseProxyHandler := func(c *gin.Context) {
-		proxy.ServeHTTP(c.Writer, c.Request)
+	port := "3000"
+	server := &http.Server{
+		Addr:      ":" + port,
+		TLSConfig: &tls.Config{},
 	}
 
-	// Define route rule, all requests are proxy to reverse proxy middleware
-	router.NoRoute(reverseProxyHandler)
+	log.Printf("Server listening on port %v", port)
+	log.Fatal(server.ListenAndServeTLS("YOUR_CERT.cert", "/root/cert/YOUR_KEY.key"))
+}
 
-	// Start HTTP server
-	if err := router.Run(":8000"); err != nil {
-		panic(err)
+func handleChat(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
 	}
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestBody RequestBody
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		log.Printf("Failed to decode request body: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Add your OpenAI API key
+	apiKey := "YOUR_API_KEY_HERE"
+
+	url := "https://api.openai.com/v1/chat/completions"
+	reqBody, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Printf("Failed to marshal request body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
+	if err != nil {
+		log.Printf("Failed to create HTTP request: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Authorization", apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Failed to send HTTP request: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to read HTTP response body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var responseBody ResponseBody
+	err = json.Unmarshal(respBody, &responseBody)
+
+	if err != nil {
+		log.Printf("Failed to unmarshal HTTP response body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse, err := json.Marshal(responseBody)
+	if err != nil {
+		log.Printf("Failed to marshal HTTP response: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
 }
